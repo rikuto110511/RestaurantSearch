ab2e6bdf350026ba96309ac2fc904d6c9c5f5957
rikuto110511 <rikuto.511@docomo.ne.jp>
2020-08-06 01:59:33 +0900

    NavBarを追加

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="ホーム" component={HomeScreen} />
+      {/* <Stack.Screen name="編集" component={EditScreen} /> */}
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <HomeStack />
+    </NavigationContainer>
+  );
+};
